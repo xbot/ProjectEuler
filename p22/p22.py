56cf2d046d824633b7f99e0b14a7eaf987d093c5
Donie Leigh <donie.leigh@gmail.com>
2014-04-21 10:58:02 +0800

    Add a second solution leveraging enumerate() for p22

--- p22/p22.py
+++ p22/p22.py
@@ -6,5 +6,12 @@ if __name__ == '__main__':
     names = sorted(f.readline().replace('"', '').split(','))
     f.close()
 
+    # Solution 1: using map() and lambda expression
+
     print sum(map(lambda name: sum([ord(c) - 64 for c in name]) \
               * (names.index(name) + 1), names))
+
+    # Solution 2: using enumerate()
+
+    print sum(sum([ord(c) - 64 for c in name]) * (i + 1) for (i,
+              name) in enumerate(names))
